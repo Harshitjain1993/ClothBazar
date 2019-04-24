677874d9b34ddd3ca586c06772e61a89aa55f3f2
Harshit Jain <jainharshit93@yahoo.in>
2019-04-24 23:39:21 +0530

    Add Category Controller with their Views for CURD operation and add a new WebService project in directory to SELECT,ADD,UPDATE,DELETE  the data in db

---
+++ ClothBazar.Web/Controllers/CategoryController.cs
@@ -0,0 +1,75 @@
+﻿using ClothBazar.Entities;
+using ClothBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesService categoryService = new CategoriesService();
+
+        // GET: Category
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var categories = categoryService.GetCategories();
+            return View(categories);
+        }
+
+
+
+        // GET: Create a Category
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // Post: Category
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoryService.SaveCategory(category);
+            return View();
+        }
+
+
+        // GET: Edit a Category
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var category = categoryService.GetCategory(ID);
+            return View(category);
+        }
+
+        // Post: Category
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            categoryService.UpdateCategory(category);
+            return RedirectToAction("Index");
+        }
+
+        // GET: Delete a Category
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            var category = categoryService.GetCategory(ID);
+            return View(category);
+        }
+
+        // Post: Category
+        [HttpPost]
+        public ActionResult Delete(Category category)
+        {
+            
+            categoryService.DeleteCategory(category.ID);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
